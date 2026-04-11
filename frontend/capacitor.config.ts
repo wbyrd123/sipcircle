5bd1efd2287535fafd386c11876e5eb5c376b46c
Wayne Byrd <waynebyrd11@gmail.com>
2026-04-11 14:16:41 +0000

    auto-commit for 5de66017-64c5-49b7-b432-284a903c3e5a

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.pourcircle.app',
+  appId: 'com.pourcircle.wb',
   appName: 'SipCircle',
   webDir: 'build',
   server: {
